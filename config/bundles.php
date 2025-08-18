238ff8d7f8ffecad27a0cee44718d768feddf417
Theo Guillaume <81827200+TheoGuillaume@users.noreply.github.com>
2025-08-18 19:10:28 +0300

    composer

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
